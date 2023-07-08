02df51b580372e4ed2757b6dab71a40191679c81
Eduardo Souza <edcampossouza@gmail.com>
2023-07-08 09:22:30 -0300

    feat: user not found exception

--- src/main/java/com/example/restfulwebservices/user/UserResource.java
+++ src/main/java/com/example/restfulwebservices/user/UserResource.java
@@ -20,7 +20,11 @@ public class UserResource {
 
     @GetMapping(path = "/users/{id}")
     public User getUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException(String.format("id: %d", id));
+        }
+        return user;
     }
 
     @PostMapping(path = "/users")
