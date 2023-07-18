063db84093db7551198c852df242f1f15c06eb32
Eduardo Souza <edcampossouza@gmail.com>
2023-07-18 18:15:59 -0300

    feat: internationaliztion with accept header locale resolver

--- src/main/java/com/example/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservices/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.example.restfulwebservices.helloworld;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
@@ -28,8 +29,9 @@ public class HelloWorldController {
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/hello-world-internationalized")
-    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-        return messageSource.getMessage("good.morning.message", null, locale);
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
+
 
 }
