f92ffc0cef6790587ce45e14c6dd6264f6c5cc1e
Eduardo Souza <edcampossouza@gmail.com>
2023-07-18 18:10:01 -0300

    feat: internationaliztion with session locale resolver

--- src/main/java/com/example/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,9 +1,17 @@
 package com.example.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
     @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     public String helloWorld() {
         return "Hello World!";
@@ -18,4 +26,10 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldName(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello, %s", name));
     }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
+    }
+
 }
