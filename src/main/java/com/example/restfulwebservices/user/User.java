e7ccc747aba44af77e6d0d5fa1ba72c0e5395c73
Eduardo Souza <edcampossouza@gmail.com>
2023-07-18 19:33:48 -0300

    feat: swagger custom annotations

--- src/main/java/com/example/restfulwebservices/user/User.java
+++ src/main/java/com/example/restfulwebservices/user/User.java
@@ -1,17 +1,23 @@
 package com.example.restfulwebservices.user;
 
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.util.Date;
 
 import javax.validation.constraints.Size;
 import javax.validation.constraints.Past;
 
+@ApiModel(description = "All details about the user")
 public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @ApiModelProperty(notes = "Name should have at least 2 characters ")
     private String name;
     @Past
+    @ApiModelProperty(notes = "Birth date should be in the past")
     private Date birthDate;
 
     public User(Integer id, String name, Date birthDate) {
