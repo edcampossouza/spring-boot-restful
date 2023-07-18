23c874eea7f98c8050fd3d687d7ef3cd1e6abc9e
Eduardo Souza <edcampossouza@gmail.com>
2023-07-17 21:14:36 -0300

    feat: validate name: custom message

--- src/main/java/com/example/restfulwebservices/user/User.java
+++ src/main/java/com/example/restfulwebservices/user/User.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.Past;
 public class User {
     private Integer id;
 
-    @Size(min = 2)
+    @Size(min = 2, message = "Name should have at least 2 characters")
     private String name;
     @Past
     private Date birthDate;
